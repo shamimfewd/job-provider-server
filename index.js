abba589cfbf399ebdfa723723735c217e035e969
shamim <shamimmahmud077@gmail.com>
2024-12-05 21:47:23 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookeParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -14,6 +16,7 @@ const corsOptions = {
 // middle ware
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookeParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ssblxww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -31,8 +34,35 @@ async function run() {
     const jobsCollection = client.db("jobProviderDB").collection("jobs");
     const bidsCollection = client.db("jobProviderDB").collection("bids");
 
-    //get a single job data from db using job id
+    // jwt generate
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
 
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
+    // clear token on logout
+    app.get("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 0,
+        })
+        .send({ success: true });
+    });
+
+    //get a single job data from db using job id
     app.get("/job/:id", async (req, res) => {
       const id = req.params.id;
       const quire = { _id: new ObjectId(id) };
@@ -63,6 +93,16 @@ async function run() {
     // get all jobs posted by a specific user
 
     app.get("/jobs/:email", async (req, res) => {
+      const token = req.cookies?.token;
+      if (token) {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+            return console.log(err);
+          }
+          console.log(decoded);
+        });
+      }
+      console.log(token);
       const email = req.params.email;
       const quire = { "buyer.email": email };
       const result = await jobsCollection.find(quire).toArray();
@@ -106,6 +146,18 @@ async function run() {
       const email = req.params.email;
       const quire = { "buyer.email": email };
       const result = await bidsCollection.find(quire).toArray();
+      res.send(result);
+    });
+
+    // update bids status
+    app.patch("/bid/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: status,
+      };
+      const result = await bidsCollection.updateOne(query, updateDoc);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
